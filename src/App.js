c85ff328cef60988a77b64e087ae7c2856560683
Tyler Jarvis <92890838+TylerJarvis8@users.noreply.github.com>
2024-09-11 11:26:55 -0700

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> Hello React</h1>
+    <div className="testUnit" style={{height: "400px", width: "400px", border: "3px solid red", borderRadius: "30px", background: "black"}}>
+      <h1 style={{color: "blue"}}>Test Unit</h1>
     </div>
   );
 }
