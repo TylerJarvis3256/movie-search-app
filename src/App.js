e26f880407e724b8f2ff368b9a6e073778961842
Tyler Jarvis <92890838+TylerJarvis8@users.noreply.github.com>
2024-09-12 12:28:59 -0700

    Final Product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import React from "react";
+import MovieCard from "./MovieCard";
+
+import "./App.css";
+import searchIcon from "./search.svg";
+const API_URL = "http://www.omdbapi.com?apikey=a854dc15";
+
+const movie1 = {
+  Title: "The Batman",
+  Year: "2022",
+  imdbID: "tt1877830",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
-    <div className="testUnit" style={{height: "400px", width: "400px", border: "3px solid red", borderRadius: "30px", background: "black"}}>
-      <h1 style={{color: "blue"}}>Test Unit</h1>
+    <div className="app">
+      <h1>CineMax</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div clasName="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
